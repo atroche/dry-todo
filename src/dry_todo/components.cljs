e46dee6abd3e62b07fdf56465c5c13e950dedfbf
Alistair Roche <roche.a@gmail.com>
2015-07-18 15:11:19 +1000

    Fix up that weird :key issue

--- src/dry_todo/components.cljs
+++ src/dry_todo/components.cljs
@@ -3,28 +3,28 @@
             [rum]
             [dry-todo.actions :refer [component-for-action]]))
 
-(defn new-todo-form [new-todo]
-  [:form {:key 1}
-   (component-for-action :fill-in-new-todo new-todo)
-   (component-for-action :add-new-todo new-todo)
-   ])
+(rum/defc new-todo-form [new-todo]
+          [:form {:key 1}
+           (component-for-action :fill-in-new-todo new-todo)
+           (component-for-action :add-new-todo new-todo)])
 
-(rum/defc todo-item [todo]
-          [:li
-           {:key (:db/id todo)}
-           (component-for-action :toggle-todo-complete todo)
-           [:span {:style {:margin 10}}
-            (:text todo)]
-           [:span (component-for-action :remove-todo todo)]])
 
 (rum/defc todo-list [todos]
           [:ul
            {:key 0}
            (for [todo todos]
-             (todo-item todo))])
+             [:li
+              {:key (:db/id todo)}
+
+              (component-for-action :toggle-todo-complete todo)
+
+              [:span {:style {:margin 10}}
+               (:text todo)]
+
+              [:span (component-for-action :remove-todo todo)]])])
 
 
 (rum/defc todo-app []
-          [:main {:key 0}
+          [:main
            (new-todo-form (get-new-todo))
            (todo-list (get-todos))])
